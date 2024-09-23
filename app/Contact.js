0dc531f8c0b9ab8db67ac66f964a33db92fe326f
Benny <bcc9902@gmail.com>
2024-09-23 17:15:35 -0400

    Update Contact.js server

--- app/Contact.js
+++ app/Contact.js
@@ -1,9 +1,6 @@
-"use client"
-import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
-import { toast } from "@/hooks/use-toast"
 import Image from "next/image"
 
 export default function Contact() {
